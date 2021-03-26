b158779c93d1b6b8f4ebf435c2f16786cb468dd5
Shivam Raj <49182562+shivam4312@users.noreply.github.com>
2021-03-26 18:33:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["an Engineer", "a Web Developer","a 3rd Year Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["an Engineer", "a Web Developer","a 3rd Year Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+// contact form backend
+
+function sendmail() {
+
+    var name = $('#Name').val();
+    var email = $('#Email').val();
+    var subject = $('#Subject').val();
+    var message = $('#Message').val();
+
+    // var body = $('#body').val();
+
+    var Body = 'Name: ' + name + '<br>Email: ' + email + '<br>Subject: ' + subject + '<br>Message: ' + message;
+    //console.log(name, phone, email, message);
+
+    Email.send({
+        SecureToken: "bacd707a-5fc6-420c-88c1-da5ed9527cc7",
+        To: 'shivam43120@gmail.com',
+        From: "shivam4312@gmail.com",
+        Subject: "New message on contact from " + name,
+        Body: Body
+    }).then(
+        message => {
+            //console.log (message);
+            if (message == 'OK') {
+                alert('Your mail has been send. Thank you for connecting.');
+            }
+            else {
+                console.error(message);
+                alert('There is error at sending message. ')
+
+            }
+
+        }
+    );
+
+}
